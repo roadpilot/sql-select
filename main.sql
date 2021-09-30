9074a7150f80a45bd5be1b380676daa8f3480751
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-30 17:01:02 +0000

    solution

--- main.sql
+++ main.sql
@@ -1,3 +1,4 @@
 /*
 Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to  decimal places.
 */
+select round(long_w, 4) from station where lat_n < 137.2345 order by lat_n desc limit 1
